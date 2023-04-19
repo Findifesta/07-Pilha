4b5bf6f865a163ec96996e89e99215cf1638d9a8
Sérgio da Rocha <kensuu@hotmail.com>
2023-04-18 23:16:37 -0300

    Funções de exibir elementos e pop inseridas. Função de inserir elementos modificada para correta execução.

--- Pilha/Pilha/Pilha.cpp
+++ Pilha/Pilha/Pilha.cpp
@@ -14,6 +14,7 @@ void menu();
 void inicializar();
 void pop();
 void push();
+void exibirElementos();
 //--------------------------
 
 
@@ -25,15 +26,15 @@ int main()
 void menu()
 {
 	int op = 0;
-	while (op != 4) {
+	while (op != 5) {
 		system("cls"); // somente no windows
 		cout << "Menu Pilha";
 		cout << endl << endl;
 		cout << "1 - Inicializar Pilha \n";
 		cout << "2 - Inserir elemento (Push) \n";
 		cout << "3 - Remover elementos (Pop) \n";
-		cout << "4 - Sair \n";
-
+		cout << "4 - Exibir elemento\n";
+		cout << "5 - Sair \n";
 
 		cout << "Opcao: ";
 		cin >> op;
@@ -46,7 +47,9 @@ void menu()
 			break;
 		case 3: pop();
 			break;
-		case 4:
+		case 4: exibirElementos();
+			break;
+		case 5:
 			return;
 		default:
 			break;
@@ -87,13 +90,49 @@ void push()
 	cin >> novo->valor;
 	novo->prox = NULL;
 
-
+	if(topo == NULL)
+	{
+		topo = novo;
+		return;
+	}
+	else
+	{
+		novo->prox = topo;
+		topo = novo;
+		return;
+	}
 }
 
 void pop()
 {
+	if(topo == NULL)
+	{
+		cout << "A pilha esta vazia.\n";
+		return;
+	}
 
-	
-
+	else
+	{
+		NO* aux = topo->prox;
+		cout << "O elemento " << topo->valor << " foi excluido da pilha.\n";
+		free(topo);
+		topo = aux;
+		return;
+	}
 }
 
+void exibirElementos()
+{
+	if (topo == NULL) {
+		cout << "Lista vazia \n";
+		return;
+	}
+	else {
+		cout << "Elementos: \n";
+		NO* aux = topo;
+		while (aux != NULL) {
+			cout << aux->valor << endl;
+			aux = aux->prox;
+		}
+	}
+}
